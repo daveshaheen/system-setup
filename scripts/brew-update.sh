aca59f4fa7d6a3fff8d06ffa8338638564984529
Dave Shaheen <dave.shaheen@gmail.com>
2016-10-09 10:03:49 -0500

    Remove --all from brew upgrade

--- scripts/brew-update.sh
+++ scripts/brew-update.sh
@@ -2,4 +2,4 @@
 # brew-update.sh
 
 echo "Updating..."
-brew update && brew upgrade --all && brew cleanup && brew cask update && brew cask install $(brew cask list) && brew cask cleanup
+brew update && brew upgrade && brew cleanup && brew cask update && brew cask install $(brew cask list) && brew cask cleanup
