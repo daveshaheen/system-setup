22ea7b4cb9e6f4dcc926851eeeec39f15d3fc230
Dave Shaheen <dave.shaheen@gmail.com>
2016-11-17 18:24:14 -0600

    Update readability

--- scripts/brew-update.sh
+++ scripts/brew-update.sh
@@ -2,4 +2,11 @@
 # brew-update.sh
 
 echo "Updating..."
-brew update && brew upgrade && brew cleanup && brew cask update && brew cask install $(brew cask list) && brew cask cleanup
+
+brew update; \
+  brew upgrade; \
+  brew cleanup; \
+  brew cask update; \
+  brew cask install $(brew cask list); \
+  brew cask cleanup; \
+  echo ""
