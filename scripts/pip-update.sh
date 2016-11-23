f8452478689e5c437109f961cacbfcd4548bc9ff
Dave Shaheen <dave.shaheen@gmail.com>
2016-11-22 22:00:06 -0600

    Update to handle both pip2 and pip3

--- scripts/pip-update.sh
+++ scripts/pip-update.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-echo ""; echo "pip update...";
+echo ""; echo "pip2 update...";
 
-pip install --upgrade pip
-pip list --outdated --format=legacy | awk '{if($1 != "GDAL" && $1 != "pip") print $1}' | xargs -L1 pip install --upgrade
+pip2 install --upgrade pip
+pip2 list --outdated --format=legacy | awk '{if($1 != "GDAL" && $1 != "pip") print $1}' | xargs -L1 pip2 install --upgrade
+
+echo ""; echo "pip3 update...";
+
+pip3 install --upgrade pip
+pip3 list --outdated --format=legacy | awk '{if($1 != "GDAL" && $1 != "pip") print $1}' | xargs -L1 pip3 install --upgrade
