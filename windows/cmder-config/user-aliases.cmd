87740740182b12c1f3a720c245c6f8c2fdcca4f6
Dave Shaheen <dave.shaheen@gmail.com>
2018-03-22 17:20:56 -0500

    Add gvim alias

--- windows/cmder-config/user-aliases.cmd
+++ windows/cmder-config/user-aliases.cmd
@@ -8,6 +8,7 @@
 clear=cls
 cmderr=cd /d "%CMDER_ROOT%"
 e.=explorer .
+gvim="C:\Program Files (x86)\Vim\vim80\gvim.exe" $*
 history=cat "%CMDER_ROOT%\config\.history"
 ll=ls -l -h --show-control-chars -F --color $*
 ls=ls --show-control-chars -F --color $*
